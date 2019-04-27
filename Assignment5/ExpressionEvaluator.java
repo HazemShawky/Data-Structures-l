10cf687fd1c3be821f6b6676e9e981aa2be87cb8
HazemShawky <42117713+HazemShawky@users.noreply.github.com>
2019-04-27 16:11:22 +0200

    Update ExpressionEvaluator.java

--- Assignment5/ExpressionEvaluator.java
+++ Assignment5/ExpressionEvaluator.java
@@ -1,6 +1,6 @@
 package eg.edu.alexu.csd.datastructure.stack.cs23;
 public class ExpressionEvaluator {
-	public static int priority(char operator) {
+	static int priority(char operator) {
 		if (operator == '*' || operator == '/')
 			return 2;
 		else if (operator == '+' || operator == '-')
@@ -8,7 +8,7 @@ public class ExpressionEvaluator {
 		else
 			return 0;
 			}
-	public static boolean checkParentheses(String expression) {
+	static boolean checkParentheses(String expression) {
 		Stack s = new Stack();
 		for (int i = 0; i < expression.length(); ++i) {
 			if(expression.charAt(i) == '(')
@@ -24,7 +24,41 @@ public class ExpressionEvaluator {
 		else
 			throw new RuntimeException("error");
 	}
-	public String infixToPostfix(String expression) {
+	static String checkMinus(String expression) {
+		boolean flag = true;
+		for (int i = 0; i < expression.length(); ++i) {
+			char c = expression.charAt(i);
+			while((c == ' ' || c == '\t') && i < expression.length()-1)
+				c = expression.charAt(++i);
+			if(c=='-' && flag) {
+				flag = false;
+				expression = expression.substring(0,i)+'!'+expression.substring(i+1);
+			}
+			if(c=='+'||c=='-'||c=='*'||c=='/'||c=='(')
+				flag = true;
+			if(Character.isLetterOrDigit(c))
+					flag = false;
+		}
+		return expression;
+	}
+	static boolean checkExpression(String expression){
+		int cnt1 = 0, cnt2 = 0;
+		for (int i = 0; i < expression.length(); ++i) {
+			char c = expression.charAt(i);
+			if(c == '+' || c == '*' || c == '/' ||c== '-')
+				cnt1++;
+			if(Character.isLetterOrDigit(c))
+				cnt2++;
+		}
+		if(cnt1+1==cnt2)
+			return true;
+		return false;
+	}
+	public static String infixToPostfix(String expression) {
+		checkParentheses(expression);
+		expression = checkMinus(expression);
+		if(!checkExpression(expression))
+			throw new RuntimeException("Enter valid expression");
 		String ans= new String("");
 		Stack s = new Stack();
 		expression += ' ';
@@ -32,11 +66,11 @@ public class ExpressionEvaluator {
 			char c = expression.charAt(i);
 			while((c == ' ' || c == '\t') && i < expression.length()-1)
 				c = expression.charAt(++i);
-			while(Character.isLetterOrDigit(c)) {
+			while(Character.isLetterOrDigit(c)  || c=='!') {
 				ans += c;
 				c = expression.charAt(++i);
 			}
-				if(ans.length() != 0 && ans.charAt(ans.length()-1) != ' ')
+				if(ans.length() != 0 && ans.charAt(ans.length()-1) != ' ' && ans.charAt(ans.length()-1) != '!')
 					ans += ' ';
 			if (c == '(') 
                 s.push(c); 
@@ -58,28 +92,29 @@ public class ExpressionEvaluator {
 	            ans += s.pop();
 	            ans += ' ';
 			}
-		return ans;
+		return ans.substring(0, ans.length()-1);
 	}
-	public int evaluate(String expression) {
-		int ans = 0;
+	public static int evaluate(String expression) {
+		float ans = 0;
 		String c = "";
+		expression += ' ';
 		Stack s = new Stack();
 		for(int i = 0; i < expression.length(); ++i) {
 			if(expression.charAt(i)==' ') {
-				if (c != "")
-					s.push(Integer.parseInt(c));
+				if (c != "") 
+					s.push((float)Integer.parseInt(c.replace('!', '-')));
 				c = "";
 			}
 			else if (expression.charAt(i) == '+' || expression.charAt(i) == '-' || expression.charAt(i) == '*'  || expression.charAt(i) == '/' )
 			{
-				int operand1 = (int)s.pop();
-				int operand2 = (int)s.pop();
+				float operand1 = (float)s.pop();
+				float operand2 = (float)s.pop();
 				switch(expression.charAt(i)) {
 				 case '+':
 					 ans += operand1 + operand2;
 					 break;
 				 case '-':
-					 ans += operand2 - operand2;
+					 ans += operand2 - operand1;
 					 break;
 				 case '*':
 					 ans += operand1 * operand2;
@@ -94,6 +129,7 @@ public class ExpressionEvaluator {
 			else
 				c += expression.charAt(i);
 		}
-		return (int)s.pop();
+		ans = (float)s.pop();
+		return (int)ans;
 	}
-  }
+}
