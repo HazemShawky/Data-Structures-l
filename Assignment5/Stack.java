7a21f90ba7fefb3c2a74af3a09d4aebb636aff46
HazemShawky <42117713+HazemShawky@users.noreply.github.com>
2019-04-27 16:13:37 +0200

    Update Stack.java

--- Assignment5/Stack.java
+++ Assignment5/Stack.java
@@ -1,4 +1,20 @@
 package eg.edu.alexu.csd.datastructure.stack.cs23;
+/**
+* Java class example
+* The class illustrates how to write comments used
+* to generate JavaDoc documentation
+*
+* @author Hazem Shawky
+* @version  27 April 2019
+*/
+
+/**
+* class defines node.
+*
+* obj contains the value of the node.
+* 
+* next points to a node next to the current node.
+*/
 class node{
 	node next;
 	Object obj;
@@ -8,6 +24,11 @@ public class Stack{
     Stack(){
 		top = null;
 	}
+    /**
+    * Removes the element at the top of stack and returns that element.
+    *
+    * @return top of stack element, or through exception if empty
+    */
 	public Object pop(){
 		if(this.isEmpty())
 			throw new RuntimeException("Stack is empty");
@@ -15,20 +36,41 @@ public class Stack{
 		top = top.next;
 		return ans;
 	}
+	/**
+	* Get the element at the top of stack without removing it from stack.
+	*
+	* @return top of stack element, or through exception if empty
+	*/
 	public Object peek() {
 		if(this.isEmpty())
 			throw new RuntimeException("Stack is empty");
 		return top.obj;
 	}
+	/**
+	* Pushes an item onto the top of this stack.
+	*
+	* @param object
+	* to insert
+	*/
 	public void push(Object element) {
 		node newNode = new node();
 		newNode.obj = element;
 		newNode.next = top;
 		top = newNode;
 	}
+	/**
+	* Tests if this stack is empty
+	*
+	* @return true if stack empty
+	*/
 	public boolean isEmpty() {
 		return top == null;
 	}
+	/**
+	* Returns the number of elements in the stack.
+	*
+	* @return number of elements in the stack
+	*/
 	public int size() {
 		int cnt = 0;
 		Stack t = new Stack();
@@ -40,4 +82,17 @@ public class Stack{
 			this.push(t.pop());
 		return cnt;
 	}
+	public static void printStack(Stack anyStack) {
+        int size = anyStack.size();
+        Stack tempStack = new Stack();
+        Object temp;
+        while (size > 0) {
+            temp = anyStack.pop();
+            System.out.print(temp + " ");
+            tempStack.push(temp);
+            size--;
+        }
+        System.out.println();
+        anyStack = tempStack;
+    }
 }
