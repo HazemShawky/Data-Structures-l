46f4423d7e970daa49e02f0197e47d99e79094fe
HazemShawky <42117713+HazemShawky@users.noreply.github.com>
2019-04-27 16:36:45 +0200

    Update ExpressionEvaluator.java

--- Assignment5/ExpressionEvaluator.java
+++ Assignment5/ExpressionEvaluator.java
@@ -1,5 +1,19 @@
 package eg.edu.alexu.csd.datastructure.stack.cs23;
+/**
+* Java class implements IExpressionEvaluator
+* This class solve infix to postfix notation and evaluate the expression.
+*
+* @author Hazem Shawky
+* @version  27 April 2019
+*/
 public class ExpressionEvaluator {
+	/**
+	* Takes a char operator and return a number represents priority
+	*
+	* @param operator
+	* char operator
+	* @return a number denotes to the priority of this operator
+	*/
 	static int priority(char operator) {
 		if (operator == '*' || operator == '/')
 			return 2;
@@ -8,6 +22,14 @@ public class ExpressionEvaluator {
 		else
 			return 0;
 			}
+	/**
+	* Takes a symbolic/numeric infix expression as input and check that the expression
+	* is valid by making sure that all open parentheses have its closing one
+	*
+	* @param expression
+	* infix expression
+	* @return true if the expression is valid
+	*/
 	static boolean checkParentheses(String expression) {
 		Stack s = new Stack();
 		for (int i = 0; i < expression.length(); ++i) {
@@ -24,6 +46,14 @@ public class ExpressionEvaluator {
 		else
 			throw new RuntimeException("error");
 	}
+	/**
+	* Takes a symbolic/numeric infix expression as input and replacing all unary '-'
+	* with '!' to find the difference between unary and binary - .
+	*
+	* @param expression
+	* infix expression
+	* @return String with '!' replacing unary '-'
+	*/
 	static String checkMinus(String expression) {
 		boolean flag = true;
 		for (int i = 0; i < expression.length(); ++i) {
@@ -41,6 +71,16 @@ public class ExpressionEvaluator {
 		}
 		return expression;
 	}
+	/**
+	* Takes a symbolic/numeric infix expression as input and check
+	* that the input is valid. There is no assumption on spaces between terms or the
+	* length of the term (e.g., two digits symbolic or numeric term) and make sure that
+	* number of operands is equal to number of operators+1
+	*
+	* @param expression
+	* infix expression
+	* @return true if the expression is valid
+	*/
 	static boolean checkExpression(String expression){
 		int cnt1 = 0, cnt2 = 0;
 		for (int i = 0; i < expression.length(); ++i) {
@@ -54,6 +94,15 @@ public class ExpressionEvaluator {
 			return true;
 		return false;
 	}
+	/**
+	* Takes a symbolic/numeric infix expression as input and converts it to
+	* postfix notation. There is no assumption on spaces between terms or the
+	* length of the term (e.g., two digits symbolic or numeric term)
+	*
+	* @param expression
+	* infix expression
+	* @return postfix expression
+	*/
 	public static String infixToPostfix(String expression) {
 		checkParentheses(expression);
 		expression = checkMinus(expression);
@@ -94,6 +143,13 @@ public class ExpressionEvaluator {
 			}
 		return ans.substring(0, ans.length()-1);
 	}
+	/**
+	* Evaluate a postfix numeric expression, with a single space separator
+	*
+	* @param expression
+	* postfix expression
+	* @return the expression evaluated value
+	*/
 	public static int evaluate(String expression) {
 		float ans = 0;
 		String c = "";
